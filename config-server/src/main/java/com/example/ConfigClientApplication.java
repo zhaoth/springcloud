46fc55c4a45ee37957e3542be9d4824913730a38
zhaoth <zhaoth@neusoft.com>
2017-03-15 10:17:58 +0800

    分布式配置

---
+++ config-server/src/main/java/com/example/ConfigClientApplication.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+//http://localhost:7001/config/prod/config-label-test
+//URL与配置文件的映射关系如下：
+//
+//		/{application}/{profile}[/{label}]
+//		/{application}-{profile}.yml
+//		/{label}/{application}-{profile}.yml
+//		/{application}-{profile}.properties
+//		/{label}/{application}-{profile}.properties
+@EnableConfigServer//开启Config Server
+@SpringBootApplication
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+}
